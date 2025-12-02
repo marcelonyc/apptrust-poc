efc9d92c2cc9f708393b00a05257915350da00d0
Marcelo Litovsky <marcelo.litovsky@gmail.com>
2025-12-02 08:39:35 -0500

    evd from bundle

--- dvr-app/app-version-from-bundle.sh
+++ dvr-app/app-version-from-bundle.sh
@@ -14,13 +14,13 @@
 # CHANGE THIS TO BE YOUR OWN VALUES!!!
 #
 source ~/.env_apptrust
-CREATE_RLM=false  # set to false to skip release bundle creation
+CREATE_RLM=true  # set to false to skip release bundle creation
 JF_PROJECT="dvr"
 APPLICATION_KEY="dvr-app"
 JFROG_CLI_BUILD_NAME='dvr-build'
-JFROG_CLI_BUILD_NUMBER=34
+JFROG_CLI_BUILD_NUMBER=32
 JFROG_CLI_BUILD_PROJECT=$JF_PROJECT
-APP_VERSION="${JFROG_CLI_BUILD_NUMBER}.7"  # CHANGE THIS TO CREATE A NEW VERSION
+APP_VERSION="${JFROG_CLI_BUILD_NUMBER}.8"  # CHANGE THIS TO CREATE A NEW VERSION
 DOCKER_REPO="dvr-docker-local-all-stages"
 IMAGE_NAME="dvr-app"
 
@@ -32,7 +32,13 @@ jf config use ${JF_CLI_INSTANCE_ID}
 
 if [ $CREATE_RLM = true ]; then
   echo "📦 Creating release bundle for version $APP_VERSION ..."
-  jf release-bundle-create --build-name dvr-build --build-number 34 dvr-app $APP_VERSION --project dvr
+  jf release-bundle-create --build-name dvr-build --build-number $JFROG_CLI_BUILD_NUMBER dvr-app $APP_VERSION --project dvr
+  if [ $? -ne 0 ]; then
+    echo "❌ Failed to create release bundle"
+    exit 1
+  else
+    echo "✅ Release bundle created successfully"
+  fi
 else
   echo "⚠️  Skipping release bundle creation"
 fi
@@ -47,13 +53,41 @@ ACTUAL_BUILD_NAME=${JFROG_CLI_BUILD_NAME}
 ACTUAL_BUILD_NUMBER=${JFROG_CLI_BUILD_NUMBER}
 RLM_REPO="dvr-release-bundles-v2"
 
-echo "🔧 Using actual build number from first job: $ACTUAL_BUILD_NUMBER"
-RLM='{
-                "name": "dvr-app",
-                "version": "34.7",
-                "repository_key": "'$RLM_REPO'"
-            }'
+# Attach evidence using JFrog CLI
+echo "📦 Attaching evidence to release bundle dvr-app:$APP_VERSION ..."
+jf evd create \
+--release-bundle "dvr-app" \
+--release-bundle-version "$APP_VERSION" \
+--project "$JF_PROJECT" \
+--predicate ./in-toto-test-clean.json \
+--predicate-type https://in-toto.io/attestation/test-result/v0.1 \
+--key "${PRIVATE_KEY}" \
+--key-alias "evd" 
+if [ $? -ne 0 ]; then
+  echo "❌ Failed to attach evidence to release bundle"
+  exit 1
+else
+  echo "✅ Evidence attached successfully to release bundle"
+fi
 
+# Create RLM JSON payload
+echo "📋 Creating RLM payload ..."
+
+RLM=$(jq -n \
+  --arg name "dvr-app" \
+  --arg version "$APP_VERSION" \
+  --arg repository_key "$RLM_REPO" \
+  '{
+    name: $name,
+    version: $version,
+    repository_key: $repository_key
+  }')
+if [ $? -ne 0 ]; then
+  echo "❌ Failed to create RLM payload"
+  exit 1
+fi
+
+echo "📋 Creating sign payload ..."
 APP_VERSION_PAYLOAD=$(jq -n \
   --arg appVersion "$APP_VERSION" \
   --arg rlmRepo "$RLM_REPO" \
